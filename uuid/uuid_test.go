9506ff967e8db1842d2ec5be9aab5fa9325e1dfc
matosiki <wx11055@163.com>
2019-07-20 17:54:54 +0800

    uuid test

--- uuid/uuid_test.go
+++ uuid/uuid_test.go
@@ -7,7 +7,7 @@ import (
 
 func BenchmarkUUID32(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		if len(_uuid.UUID32()) != 32 {
+		if len(uuid.UUID32()) != 32 {
 			b.Errorf("%s", "生成的uuid长度不为32")
 		}
 	}
